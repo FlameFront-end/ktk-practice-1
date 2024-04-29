53a80a30f77876a4c7ff2756feabb7178bf97193
Flame <5017_30@mail.ru>
2024-04-30 01:14:45 +0300

    fix build

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,19 +5,22 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.scss'
 import { SnackbarProvider } from 'notistack'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import Register from './pages/Register/Register.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<BrowserRouter>
-		<SnackbarProvider
-			anchorOrigin={{
-				vertical: 'top',
-				horizontal: 'right'
-			}}
-			maxSnack={2}
-			autoHideDuration={2000}
-		>
+	<SnackbarProvider
+		anchorOrigin={{
+			vertical: 'top',
+			horizontal: 'right'
+		}}
+		maxSnack={2}
+		autoHideDuration={2000}
+	>
+		<BrowserRouter>
 			<App />
-		</SnackbarProvider>
-	</BrowserRouter>
+		</BrowserRouter>
+	</SnackbarProvider>
 )
