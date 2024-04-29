6a531fad84a054473f6537bb51fa8e5047915754
Flame <5017_30@mail.ru>
2024-04-30 01:25:41 +0300

    fix build [2]

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 import './index.scss'
-import { SnackbarProvider } from 'notistack'
-import Home from './pages/Home/Home.jsx'
-import Login from './pages/Login/Login.jsx'
-import Register from './pages/Register/Register.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-	<SnackbarProvider
-		anchorOrigin={{
-			vertical: 'top',
-			horizontal: 'right'
-		}}
-		maxSnack={2}
-		autoHideDuration={2000}
-	>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</SnackbarProvider>
-)
+root.render(<App />)
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home/Home.jsx'
 import Login from '../pages/Login/Login.jsx'
 import Register from '../pages/Register/Register.jsx'
 
 const AppRoutes = () => {
 	return (
-		<Routes>
-			<Route element={<Home />} path='/' />
-			<Route element={<Login />} path='/login' />
-			<Route element={<Register />} path='/register' />
-		</Routes>
+		<HashRouter>
+			<Routes>
+				<Route element={<Home />} path='/' />
+				<Route element={<Login />} path='/login' />
+				<Route element={<Register />} path='/register' />
+			</Routes>
+		</HashRouter>
 	)
 }
 
